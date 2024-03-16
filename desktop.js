ed0f5fdd9977d25c17fb3c350f3291451e2f0b02
jorbinjubin <orangestickfigure123@gmail.com>
2024-03-15 22:50:36 -0400

    wow it kinda works

--- desktop.js
+++ desktop.js
@@ -1,14 +1,46 @@
-const c = document.getElementById("desktop").getContext("2d");
-c.fillStyle="#bbbbbb"
-c.width=window.innerWidth
-c.height=window.innerHeight
-window.addEventListener('mousemove', mousemove);
-let cursor = {x:0, y:0}
+const canvas = document.getElementById("desktop");
+const c = canvas.getContext("2d");
+c.fillStyle="#bbbbbb";
 
-// Create a function to track cursor position
+// Variables
+let isHolding = false;
+let cursor = {
+    x:0,
+    y:0,
+    lastX:0,
+    lastY:0,
+    held:false
+};
+
+// Event Listeners
+window.addEventListener('mousemove', mousemove);
+window.addEventListener("mousedown", mousedown);
+window.addEventListener("mouseup", mouseup);
+
+// Event Functions
+function mousedown(event) {
+    cursor.held = true;
+}
+function mouseup(event) {
+    cursor.held = false;
+}
 function mousemove(event) {
-    cursor.x=event.pageX;
-    cursor.y=event.pageY;
-    c.fillRect(cursor.x-10, cursor.y-10, cursor.x+250, cursor.y+150);
+    clear();
+    cursor.x = event.pageX;
+    cursor.y = event.pageY;
 }
 
+requestAnimationFrame(draw);
+function draw() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    if(cursor.held) {
+        c.fillStyle = "#bbbbbb";
+        c.fillRect(cursor.x, cursor.y, 300, 225);
+    }
+    requestAnimationFrame(draw);
+}
+function clear() {
+    c.fillStyle = "DarkCyan";
+    c.fillRect(0,0,c.canvas.width, c.canvas.height);
+}
