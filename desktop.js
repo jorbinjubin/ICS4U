833c8ac7daa7ba3bb5cb5da71bcc7e4178be508e
jorbinjubin <orangestickfigure123@gmail.com>
2024-03-15 23:25:06 -0400

    setting up framework for blah

--- desktop.js
+++ desktop.js
@@ -7,11 +7,32 @@ let isHolding = false;
 let cursor = {
     x:0,
     y:0,
-    lastX:0,
-    lastY:0,
+    lastx:0,
+    lasty:0,
     held:false
 };
-
+let container =  {
+    x:0,
+    y:0,
+    title:"",
+    width:0,
+    height:0,
+    minimized:true,
+    hovered:false,
+    selected:false,
+    open: function() {
+        //drawContainer(this.x, this.y, this.width, this.height, this.title);
+    },
+    close: function() {
+        // later lolol
+    }
+};
+let icon = {
+    x:0,
+    y:0,
+    image:"",
+    hovered:false
+}
 // Event Listeners
 window.addEventListener('mousemove', mousemove);
 window.addEventListener("mousedown", mousedown);
@@ -20,23 +41,35 @@ window.addEventListener("mouseup", mouseup);
 // Event Functions
 function mousedown(event) {
     cursor.held = true;
+    cursor.lastx = cursor.x;
+    cursor.lasty = cursor.y;
 }
 function mouseup(event) {
     cursor.held = false;
+    cursor.lastx = cursor.x;
+    cursor.lasty = cursor.y;
 }
 function mousemove(event) {
-    clear();
     cursor.x = event.pageX;
     cursor.y = event.pageY;
+    if(cursor.held) {
+        cursor.x = event.pageX;
+        cursor.y = event.pageY;
+    }
 }
 
 requestAnimationFrame(draw);
 function draw() {
+    clear();
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     if(cursor.held) {
         c.fillStyle = "#bbbbbb";
         c.fillRect(cursor.x, cursor.y, 300, 225);
+    }
+    else {
+        c.fillStyle = "#a6a6a6";
+        c.fillRect(cursor.lastx, cursor.lasty, 300, 225);
     }
     requestAnimationFrame(draw);
 }
